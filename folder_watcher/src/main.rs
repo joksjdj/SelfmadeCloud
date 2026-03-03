2005e6edc5429fd377468200823216ef7c62926c
joksjdj <alexanderto2008@gmail.com>
2026-03-03 11:38:57 +0100

    something

--- folder_watcher/src/main.rs
+++ folder_watcher/src/main.rs
@@ -1,7 +1,17 @@
 use watcher::{print_test, Time};
+use std::path::Path;
 
 fn main() {
     let t = Time::run();
     
     t(|| print_test("Hello from lib.rs"));
+
+    let path = Path::new("data");
+
+    if path.exists() {
+        println!("Data folder exists!");
+    } else {
+        println!("Data folder missing!");
+    }
+
 }
